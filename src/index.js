bf6c1b4c5da1e41b85b48b9eb564b646c4fbe949
Michael Ballard <ballard.michael9@gmail.com>
2020-11-29 20:14:22 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -73,6 +73,7 @@ document.addEventListener("DOMContentLoaded", () => {
          })
      })
      .then( res => res.json())
+     .then( data => console.log(data);)
      .catch( err => console.log(err))
 
  })
