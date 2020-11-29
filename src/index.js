aa1967c17a020c07620ffe14173686433ac82e17
Michael Ballard <ballard.michael9@gmail.com>
2020-11-29 20:36:13 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -81,7 +81,7 @@ toyCollection.addEventListener('click', (e) => {
 
     if(e.target.classList[0] === 'like-btn'){
 
-        console.log(e.target.previousSibling.innerText)
+        console.log(e.target.previousSibling.textContent)
         // let url = 'http://localhost:3000/toys/:id'
         // fetch(url, {
         //     method: 'PATCH',
